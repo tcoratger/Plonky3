1fc17a08e090fbe04179dc03281d85ccb71a01ef
kilic <onurkilic1004@gmail.com>
2026-05-28 19:34:12 +0200

    rm proof

--- fri/src/proof.rs
+++ fri/src/proof.rs
@@ -4,31 +4,6 @@ use p3_commit::Mmcs;
 use p3_field::Field;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone)]
-#[serde(bound(
-    serialize = "Witness: Serialize, InputProof: Serialize",
-    deserialize = "Witness: Deserialize<'de>, InputProof: Deserialize<'de>"
-))]
-pub struct FriProof<F: Field, M: Mmcs<F>, Witness, InputProof> {
-    pub commit_phase_commits: Vec<M::Commitment>,
-    pub commit_pow_witnesses: Vec<Witness>,
-    pub query_proofs: Vec<QueryProof<F, M, InputProof>>,
-    pub final_poly: Vec<F>,
-    pub query_pow_witness: Witness,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-#[serde(bound(
-    serialize = "InputProof: Serialize",
-    deserialize = "InputProof: Deserialize<'de>",
-))]
-pub struct QueryProof<F: Field, M: Mmcs<F>, InputProof> {
-    pub input_proof: InputProof,
-    /// For each commit phase commitment, this contains openings of a commit phase codeword at the
-    /// queried location, along with an opening proof.
-    pub commit_phase_openings: Vec<CommitPhaseProofStep<F, M>>,
-}
-
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(bound = "")]
 pub struct CommitPhaseProofStep<F: Field, M: Mmcs<F>> {
